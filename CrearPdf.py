779f9d0af11bbaf9de74373054e4e80aecbc788f
Rodrigo Reyes <rodrigo.reyes@fcrengineering.cl>
2025-04-04 12:14:49 -0300

    creacion de pdf version windows

--- CrearPdf.py
+++ CrearPdf.py
@@ -5,10 +5,31 @@ def crea_pdf(ruta_template, info, rutacss=''):
     nombre_template = ruta_template.split('/')[-1]
     ruta_template = ruta_template.replace(nombre_template, '')
 
-    print(nombre_template)
-    print(ruta_template)
+    print("nombre_template: ", nombre_template)
+    print("ruta_template: ", ruta_template)
+
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(ruta_template))
+    template = env.get_template(nombre_template)
+    html = template.render(info)
+
+    print(html)
+
+    options = {
+        'page-size': 'Letter',
+        'margin-top': '0.05in',
+        'margin-right': '0.05in',
+        'margin-bottom': '0.05in',
+        'margin-left': '0.05in',
+        'encoding': 'UTF-8'
+    }
+
+    config = pdfkit.configuration(wkhtmltopdf='C:/Program Files/wkhtmltopdf/bin/wkhtmltopdf.exe')
+
+    ruta_salida = 'C:/Users/rodre/source/repos/generador-pdf/reconocimiento_python.pdf'
+
+    pdfkit.from_string(html, ruta_salida, css=rutacss, options=options, configuration=config)
 
 if __name__ == "__main__":
-    ruta_template = 'C:\\Users\\rodre\\source\\repos\\generador-pdf\\template.html'
-    info = {}
+    ruta_template = 'C:/Users/rodre/source/repos/generador-pdf/template.html'
+    info = {"nombreAlumno": "Fernando Cortés", "nombreCurso": "Introducción a Python", "fecha": "2023-10-01"}
     crea_pdf(ruta_template, info)
