45709c00a6273667602e0000edcc47977f4db86d
roinflames <rod.reyes.s@gmail.com>
2025-05-08 16:27:45 -0400

    Agrego tips de posible mejora e implementación de gestión documental

--- README.md
+++ README.md
@@ -11,3 +11,14 @@ Migrar tus scripts app.py a routes.py en cada módulo.
 Crear la autenticación mínima con SQLite o una base simple.
 Generarte una plantilla de login básica.
 Armar el Pipfile o requirements.txt.
+
+# Pendientes
+ID_DOCUMENTO-00_Folio cliente_Código nemotécnico
+
+Tipos de roles (3-4)
+Generar documento
+
+C:
+T:Tesorería
+F:Familia
+
--- app/models.py
+++ app/models.py
@@ -6,7 +6,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
-
+    # roles = db.Column(db.String(50), nullable=False)  # TODO 'admin', 'user', etc. basico, medio, avanzado
+    
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
